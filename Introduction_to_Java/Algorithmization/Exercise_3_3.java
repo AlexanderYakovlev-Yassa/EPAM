75c9d1d7a5d01b7eadf6cbf568c1c76b775228a1
Alexander Yakovlev <47533457+AlexanderYakovlev-Yassa@users.noreply.github.com>
2019-07-30 15:37:30 +0300

    Add files via upload

---
+++ Introduction_to_Java/Algorithmization/Exercise_3_3.java
@@ -0,0 +1,23 @@
+/*Дана последовательность чисел.Требуется переставить элементы так,
+чтобы они были расположены по убыванию. Для этого в массиве, начиная с первого, выбирается наибольший
+элемент и ставится на первое место, а первый - на место наибольшего. Затем, начиная со второго, эта процедура
+повторяется. Написать алгоритм сортировки выбором.*/
+
+public class Exercise_3_3 {
+
+    public static void main(String[] args) {
+        double[] a = {0.0, 2.0, 1.5, 25, 63, 0.587, -58.0, 3.8, -3.6};
+        System.out.println(max(a,0));
+    }
+
+
+
+    //the search the max starts from the element with index f and returns index of it;
+    public static int max(double[] array, int f) {
+        int max = f;
+        for (int i = f + 1; i < array.length; i++) {
+            max = array[max] < array[i] ? i : max;
+        }
+        return max;
+    }
+}
