de654353845e1e8b02165b75fd19a3882819ffed
Alexander Yakovlev <47533457+AlexanderYakovlev-Yassa@users.noreply.github.com>
2019-08-08 21:31:41 +0300

    Add files via upload

--- Introduction_to_Java/Algorithmization/Exercise_2_4_9.java
+++ Introduction_to_Java/Algorithmization/Exercise_2_4_9.java
@@ -12,20 +12,60 @@ public class Exercise_2_4_9 {
 
     }
 
-    public static double areaOfQuadrangle(double[] sides){
-        double area = 0;
+    public static double[] areaOfQuadrangle(double[] sides) {
+        double[] area = new double[2];
         double p = (sides[0] + sides[1] + sides[2] + sides[3]) / 2;
-        area = Math.sqrt((p - sides[0]) * (p - sides[1]) *(p - sides[2]) *(p - sides[3]));
+        //area = Math.sqrt((p - sides[0]) * (p - sides[1]) *(p - sides[2]) *(p - sides[3]));
+        double x = sides[0];
+        double y = sides[1];
+        double z = sides[2];
+        double t = sides[3];
+        double areaOf_X_Y_SidesTriangle = areaOfRightTriangle(x, y);
+        double e = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));//hypotenuse of triangle X Y E
+        double areaOf_Z_T_SidesTriangle = areaOfTriangle(z, t, e);
+        double areaOfConvexQuadrangle = areaOf_X_Y_SidesTriangle + areaOf_Z_T_SidesTriangle;
+
+        double areaOfSelfIntersectingQuadrangle = 0;
+        double angleAlphaSinus = x / e;
+        double angleBetaSinus = y / e;
+        double k = Math.pow(e, 2) - Math.pow(t, 2) + Math.pow(z, 2);
+        double h = Math.sqrt(Math.pow(z, 2) - Math.pow(k / (2 * e), 2));
+        double angleHammaSinus = h / z;
+        double angleDeltaSinus = h / t;
+        if ((angleAlphaSinus > angleDeltaSinus) && (angleBetaSinus > angleHammaSinus)) {
+            double areaOfNonConvexQuadrangle = areaOf_X_Y_SidesTriangle - areaOf_Z_T_SidesTriangle;
+        } else if ((angleAlphaSinus < angleDeltaSinus) && (angleBetaSinus < angleHammaSinus)) {
+            double areaOfNonConvexQuadrangle = areaOf_Z_T_SidesTriangle - areaOf_X_Y_SidesTriangle;
+        } else if ((angleAlphaSinus > angleDeltaSinus) && (angleBetaSinus < angleHammaSinus)){
+            double angleAlpfa = Math.asin(angleAlphaSinus);
+            double angleDelta = Math.asin(angleDeltaSinus);
+            double angleMu = angleAlpfa - angleDelta;
+            double areaOfFirstTriangle = areaOfTriangleSideAndTwoAngles(y,angleMu,Math.PI/2);
+            double angleNu = Math.PI/2-angleMu;
+            double angleBeta = Math.asin(angleBetaSinus);
+            double angleHamma = Math.asin(angleHammaSinus);
+            double angleFi = angleHamma - angleBeta;
+            double angleEpsilon = Math.PI - angleFi - angleNu;
+            double areaOfSecondTriangle = areaOfTriangleSideAndTwoAngles(z,angleEpsilon,angleFi);
+            double areaOfSelfCrossedQuadrangle = areaOfFirstTriangle + areaOfSecondTriangle;
+        }
+
+        //area = ;
         return area;
     }
+    //returns area of triangle (angle-side-angle)
+    public static double areaOfTriangleSideAndTwoAngles(double side, double alpha, double beta){
+        double area = Math.pow(side, 2) / 2 * Math.sin(alpha) * Math.sin(beta)/ Math.sin(alpha + beta);
+        return  area;
+    }
 
-    //returns the area of a right triangle in two legs
+    //returns the area of a right triangle (side-right angle-side)
     public static double areaOfRightTriangle(double leg_1, double leg_2) {
         double area = leg_1 * leg_2 / 2;
         return area;
     }
 
-    //returns the area of a triangle on three sides
+    //returns the area of a triangle (side-side-side)
     public static double areaOfTriangle(double side_a, double side_b, double side_c) {
         double p = (side_a + side_b + side_c) / 2;
         double area = Math.sqrt(p * (p - side_a) * (p - side_b) * (p - side_c));
