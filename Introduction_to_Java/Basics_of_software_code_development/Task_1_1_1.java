f9d58a6fe4611c06355af17395793a75ba0384b6
Alexander Yakovlev <47533457+AlexanderYakovlev-Yassa@users.noreply.github.com>
2019-08-12 10:18:26 +0200

    Update and rename Exercise_1_1_1.java to Task_1_1_1.java

--- Introduction_to_Java/Basics_of_software_code_development/Task_1_1_1.java
+++ Introduction_to_Java/Basics_of_software_code_development/Task_1_1_1.java
@@ -1,6 +1,6 @@
 // Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.
 
-public class Exercise_1_1 {
+public class Task_1_1_1 {
     public static void main(String[] args) {
         System.out.println(zFunction(5, 3, 58));
     }
