06b6e2913e4c4b73774542c9be53fdc646724907
Alexander Yakovlev <47533457+AlexanderYakovlev-Yassa@users.noreply.github.com>
2019-08-29 15:42:34 +0300

    Add files via upload

--- TEMPORARY/Time.java
+++ TEMPORARY/Time.java
@@ -9,19 +9,41 @@ public class Time {
     }
 
     public Time(int value) {
-        value = value < 0 ? 0 : value;
-        this.value = value % 86400;
+        value = value % 86400;
+        this.value = value < 0 ? 86400 + value : value;
     }
 
-    /*public Time(String strTime) {
-        Pattern p = Pattern.compile("((^[\\d]{2})[:]([\\d]{2})[:]([\\d]{2}$))");
+    public Time(int hours, int minutes, int seconds) {
+        this.setTime(hours, minutes, seconds);
+    }
+
+    public Time(String strTime) {
+        setTime(strTime);
+    }
+
+    public void setTime(int hours, int minutes, int seconds) {
+        this.setHours(hours);
+        this.setMinutes(minutes);
+        this.setSeconds(seconds);
+    }
+
+    public void setTime(String strTime) {
+        Pattern p = Pattern.compile("^(\\d\\d?):(\\d\\d?):(\\d\\d?)$");
         Matcher m = p.matcher(strTime);
-        int[] time = new int[3];
-        time[0] = Integer.valueOf(m.group(1));
-        time[1] = Integer.valueOf(m.group(2));
-        time[2] = Integer.valueOf(m.group(3));
-        value = collectTime(time);
-    }*/
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+        if (m.find()) {
+            hours = Integer.valueOf(m.group(1));
+            minutes = Integer.valueOf(m.group(2));
+            seconds = Integer.valueOf(m.group(3));
+        } else {
+
+        }
+        this.setHours(hours);
+        this.setMinutes(minutes);
+        this.setSeconds(seconds);
+    }
 
     public void setHours(int hour) {
         int[] time = separateTime(value);
@@ -29,7 +51,7 @@ public class Time {
         value = collectTime(time);
     }
 
-    public void setMinuts(int minutes) {
+    public void setMinutes(int minutes) {
         int[] time = separateTime(value);
         time[1] = (minutes < 60) && (minutes > 0) ? minutes : 0;
         value = collectTime(time);
@@ -49,15 +71,13 @@ public class Time {
 
     public void addMinutes(int minutes) {
         int[] time = separateTime(value);
-        time[0] += minutes/60;
-        time[1] += minutes % 60;
+        time[1] += minutes % 1440;
         value = collectTime(time);
     }
 
     public void addSeconds(int seconds) {
         int[] time = separateTime(value);
-        time[1] += seconds/60;
-        time[2] += seconds % 60;
+        time[2] += seconds % 86400;
         value = collectTime(time);
     }
 
@@ -71,38 +91,40 @@ public class Time {
     private int[] separateTime(int value) {
         int[] time = new int[3];
         time[0] = value / 3600;
-        time[1] = (value - time[0] * 3600) / 60;
-        time[2] = value - time[0] * 3600 - time[1] * 60;
+        time[1] = value % 3600 / 60;
+        time[2] = value % 60;
         return time;
     }
 
     private int collectTime(int[] separatedTime) {
-        separatedTime[0] = (separatedTime[0] % 24 + (separatedTime[1] / 60) + (separatedTime[2] / 3600)) % 24;
-        separatedTime[1] = (separatedTime[1] % 60 + (separatedTime[2] / 60)) % 60;
-        separatedTime[2] = separatedTime[2] % 60;
-        int time = (separatedTime[0] % 24) * 3600 + (separatedTime[1] % 60) * 60 + separatedTime[2];
+        int time = separatedTime[0] * 3600 + separatedTime[1] * 60 + separatedTime[2];
         time = time % 86400;
         time = time < 0 ? 86400 + time : time;
         return time;
     }
 
+    public int compare(Time time) {
+        int res;
+        if (this.value > time.value) {
+            res = 1;
+        } else if (this.value < time.value) {
+            res = -1;
+        } else {
+            res = 0;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         Time t = new Time();
         System.out.println(t.showTime());
-        t.setHours(1);
+        t.addHours(-5);
         System.out.println(t.showTime());
-        t.setMinuts(55);
+        Time t1 = new Time("12:58:57");
+        Time t2 = new Time("00:00:00");
+        System.out.println(t1.showTime() + "\n" + t2.showTime() + "\n" + t1.compare(t2));
+        t.setTime("12:15:52");
         System.out.println(t.showTime());
-        t.setSeconds(58);
-        System.out.println(t.showTime());
-        t.addHours(2);
-        System.out.println(t.showTime());
-        t.addMinutes(5);
-        System.out.println(t.showTime());
-        t.addSeconds(-59);
-        System.out.println(t.showTime());
-        //Time tt = new Time("12:54:03");
-        //System.out.println(tt.showTime());
     }
 
 
--- TEMPORARY/Train.java
+++ TEMPORARY/Train.java
@@ -1,11 +1,18 @@
 public class Train {
     private String destination;
     private int trainNum;
-    private  int departureTime;
+    private  Time departureTime;
 
-    public Train(int trainNum, String destination, int departureTime) {
+    public Train(int trainNum, String destination, Time departureTime) {
         this.destination = destination;
         this.trainNum = trainNum;
         this.departureTime = departureTime;
     }
+
+    public String showTrain(){
+        String res;
+        res = "N " + trainNum + "to: " + destination + " " + departureTime.showTime();
+        return res;
+    }
+
 }
