43e63b906481da1ea8e9affa7e91b693473cd42b
Alexander Yakovlev <47533457+AlexanderYakovlev-Yassa@users.noreply.github.com>
2019-08-30 14:49:06 +0300

    Add files via upload

--- TEMPORARY/Train.java
+++ TEMPORARY/Train.java
@@ -1,7 +1,7 @@
 public class Train {
     private String destination;
     private int trainNum;
-    private  Time departureTime;
+    private Time departureTime;
 
     public Train(int trainNum, String destination, Time departureTime) {
         this.destination = destination;
@@ -9,10 +9,58 @@ public class Train {
         this.departureTime = departureTime;
     }
 
-    public String showTrain(){
+    public Train() {
+        this.destination = "";
+        this.trainNum = 0;
+        this.departureTime = new Time();
+    }
+
+    public int getTrainNum() {
+        return trainNum;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public Time getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setTrainNum(int trainNum) {
+        this.trainNum = trainNum;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setDepartureTime(Time departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String showTrain() {
         String res;
-        res = "N " + trainNum + "to: " + destination + " " + departureTime.showTime();
+        res = String.format("   %-7s %-18s %s", trainNum, destination, departureTime.showTime());
         return res;
+    }
+
+    public void copyTo(Train train) {
+        train.setDepartureTime(departureTime);
+        train.setDestination(destination);
+        train.setTrainNum(trainNum);
+    }
+
+    public int compareDepartureTime(Train train) {
+        return departureTime.compare(train.departureTime);
+    }
+
+    public int compareDestinations(Train train) {
+        return destination.compareTo(train.destination);
+    }
+
+    public int compareTrainNumbers(Train train) {
+        return trainNum - train.trainNum;
     }
 
 }
